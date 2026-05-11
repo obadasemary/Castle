7cdef9e67302b497119e6c0ee203402cd5eb24e8
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-05-11 12:08:46 +0300

    Initial Commit

---
+++ Castle/CastleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastleApp.swift
+//  Castle
+//
+//  Created by Abdelrahman Mohamed on 11.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CastleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
